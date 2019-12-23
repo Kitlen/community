6ecaba2e08cab2d2f4687fd368463658ed98e08c
kitlen <kitWT2019@163.com>
2019-12-23 16:19:28 +0800

    init repo

---
+++ src/main/java/life/majiang/community/controller/HelloController.java
@@ -0,0 +1,28 @@
+package life.majiang.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * kitlen All rights reserved.
+ * <p>
+ * Author: kitlen
+ * Version: 1.0
+ * Created Time: 2019-12-20
+ * <p>
+ * Revision History:
+ * Version          Date               Author			Comments
+ * 1.0         	2019-12-20		kitlen			Create file
+ * =========================================================================
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String greeting(@RequestParam(name="name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
